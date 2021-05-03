0da95a17e2e7e59279a2110e6ff919c9e930b844
Dhruva <todhruva@mail.ru>
2021-05-03 15:11:04 +0300

    add vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,20 @@
-const path = require('path');
+//const path = require('path');
 
 module.exports = {
-	
-	parallel: false,
-	outputDir: 'docs',
-	publicPath: process.env.NODE_ENV === "production" ? "/liquid-trader/" : "/liquid-trader",
-	"devServer": {
+    parallel: false,
+    outputDir: 'docs',
+    publicPath: process.env.NODE_ENV === "production" ? "/liquid-trader/" : "/liquid-trader",
+
+    "devServer": {
 		"port": 8383
 	},
-	css: {
+
+    css: {
         requireModuleExtension: true,
         extract: true
     },
+
+    transpileDependencies: [
+      'vuetify'
+    ]
 }    
