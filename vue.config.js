0a11f28b1d649961de9d245e0ebcc509dc13f5a4
Dhruva <todhruva@mail.ru>
2021-05-03 15:01:22 +0300

    init

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+	
+	parallel: false,
+	outputDir: 'docs',
+	publicPath: process.env.NODE_ENV === "production" ? "/liquid-trader/" : "/liquid-trader",
+	"devServer": {
+		"port": 8383
+	},
+	css: {
+        requireModuleExtension: true,
+        extract: true
+    },
+}    
