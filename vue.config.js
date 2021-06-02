0ba471c8b8b073a180e16d5764ff7caad6310ae6
Dhruva <todhruva@mail.ru>
2021-06-02 17:40:56 +0300

    Publish again change path

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@
 module.exports = {
     parallel: false,
     outputDir: 'docs',
-    publicPath: process.env.NODE_ENV === "production" ? "/" : "/liquid-trader",
+    publicPath: process.env.NODE_ENV === "production" ? "/post-ops/" : "/liquid-trader",
 
     "devServer": {
 		"port": 8383
