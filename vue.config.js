5e98a7ba0cfaf218de682224809944f726c421b8
Dhruva <todhruva@mail.ru>
2021-06-03 13:36:19 +0300

    remove asset root

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,9 @@ process.env.VUE_APP_VERSION = process.env.npm_package_version
 module.exports = {
     parallel: false,
     outputDir: 'docs',
-    assetsDir: 'post-ops',
+   // assetsDir: 'post-ops',
     filenameHashing:false,
-    publicPath: process.env.NODE_ENV === "production" ? "/" : "/post-ops/",
+    publicPath: process.env.NODE_ENV === "production" ? "/post-ops/" : "/post-ops/",
 
     "devServer": {
 		"port": 8383
